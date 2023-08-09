8d73c5a88e43af4dfdd93549f9e5c1924691e33a
arthurfoschiani <arthur.foschiani@outlook.com>
2023-08-09 20:58:13 -0300

    xxxx

--- src/main/java/br/com/fiap/Main.java
+++ src/main/java/br/com/fiap/Main.java
@@ -6,9 +6,5 @@ import jakarta.persistence.Persistence;
 public class Main {
     public static void main(String[] args) {
 
-
-        
-
-
     }
 }
---
+++ src/main/java/br/com/fiap/domain/entity/Deposito.java
@@ -0,0 +1,47 @@
+package br.com.fiap.domain.entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "TB_DEPOSITO")
+public class Deposito {
+    @Id
+    @SequenceGenerator(name = "SQ_DEPOSITO", sequenceName = "SQ_DEPOSITO", allocationSize = 1, initialValue = 1)
+    @GeneratedValue(strategy = GenerationType.SEQUENCE, generator = "SQ_DEPOSITO")
+    private Long id;
+    @Column(name = "NM_DEPOSITO")
+    private String name;
+    public Deposito() {
+    }
+
+    public Deposito(Long id, String name) {
+        this.setId(id);
+        this.setName(name);
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public Deposito setId(Long id) {
+        this.id = id;
+        return this;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public Deposito setName(String name) {
+        this.name = name;
+        return this;
+    }
+
+    @Override
+    public String toString() {
+        return "Deposito{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                '}';
+    }
+}
